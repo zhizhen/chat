0bb50fa4de5d1bf371d1778efd4d6bc1017631e9
zhizhen <zhzhzhen@gmail.com>
2016-11-12 10:44:02 +0800

    config bug

--- src/chat.erl
+++ src/chat.erl
@@ -5,6 +5,6 @@
 load() ->
     %chat_backend:load(),
     chat_mod_sync:load([]),
-    %chat_mod_message:load([]),
+    chat_mod_message:load([]),
    % chat_mod_presence:load([]),
     ok.
--- src/chat_mod_message.erl
+++ src/chat_mod_message.erl
@@ -9,20 +9,25 @@
 -export([load/1, message_published/2, message_acked/3, unload/1]).
 
 load(Opts) ->
-    emqttd_broker:hook('message.publish', {?MODULE, slimchat_message_published},
-                       {?MODULE, message_published, [Opts]}),
-    emqttd_broker:hook('message.acked', {?MODULE, slimchat_message_acked},
-                       {?MODULE, message_acked, [Opts]}).
+    emqttd:hook('message.publish', fun ?MODULE:message_published/2, [Opts]),
+    emqttd:hook('message.acked', fun ?MODULE:message_acked/3, [Opts]).
+
+message_published(Message = #mqtt_message{id = MsgId, qos = 1,
+                                          from = ClientId,
+                                          topic = Topic = <<"chat/", _To/binary>>},
+                  _Opts) ->
+    lager:info("chat message : ~p~n", [{Message}]),
+    {ok, Message};
 
 message_published(Message, _Opts) ->
-    lager:info("unhandled message published : ~p~n", [{Message, _Opts}]),
-    Message.
+    %lager:info("unhandled message published : ~p~n", [{Message, _Opts}]),
+    {ok, Message}.
 
 message_acked(_ClientId, _Message, _Opts) ->
     lager:info("unhandled message acked : ~p~n", [{_ClientId, _Message, _Opts}]),
     ok.
 
 unload(_Opts) ->
-    emqttd_broker:unhook('message.publish', {?MODULE, slimchat_message_published}),
-    emqttd_broker:unhook('message.acked', {?MODULE, slimchat_message_acked}).
+    emqttd:unhook('message.publish', fun ?MODULE:message_published/2),
+    emqttd:unhook('message.acked', fun ?MODULE:message_acked/3).
 
